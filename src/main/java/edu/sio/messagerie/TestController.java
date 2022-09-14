06303b32c8eb63d717a56af58205120105a410d4
J-C HERON <myaddressmail@gmail.com>
2022-09-14 16:08:19 +0200

    add update orga

--- src/main/java/edu/sio/messagerie/TestController.java
+++ src/main/java/edu/sio/messagerie/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -29,17 +29,26 @@ public class TestController {
 	}
 
 	@GetMapping("/orga/addNew")
-	public String addNew() {
-		return "/orga/addNew";
+	public String addNew(ModelMap model) {
+		model.addAttribute("orga", new Organization());
+
+		return "/orga/orgaForm";
 	}
 
-	@PostMapping("/orga/addNew")
-	public RedirectView addNewSubmit(@RequestParam String name, RedirectAttributes attrs) {
-		Organization orga = new Organization();
-		orga.setName(name);
+	@PostMapping("/orga/addOrUpdate")
+	public RedirectView addNewSubmit(@RequestBody Organization orga, RedirectAttributes attrs) {
 		repository.save(orga);
-		attrs.addFlashAttribute("msg", "Organisation ajoutée : <strong>" + orga + "</strong>");
+		attrs.addFlashAttribute("msg", "Organisation : <strong>" + orga + "</strong>");
 		return new RedirectView("/orga");
+	}
+
+	@PostMapping("/orga/update/{id}")
+	public String updateForm(ModelMap model, @PathVariable int id) {
+		Optional<Organization> optOrga = repository.findById(id);
+		if (optOrga.isPresent()) {
+			model.addAttribute("orga", optOrga.get());
+		}
+		return "/orga/orgaForm";
 	}
 
 	@PostMapping("/orga/delete/{id}")
